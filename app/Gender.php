c5f37d61c47fb595728071611879507ca7dcfa10
Administrador <edgar.yerena@metodika.mx>
2018-08-18 10:53:27 -0500

    Initial project config

---
+++ app/Gender.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ReclutaTI;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gender extends Model
+{
+    const FEMALE = 1;
+    const MALE = 2;
+}
--- config/app.php
+++ config/app.php
@@ -154,11 +154,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        ReclutaTI\Providers\AppServiceProvider::class,
+        ReclutaTI\Providers\AuthServiceProvider::class,
+        // ReclutaTI\Providers\BroadcastServiceProvider::class,
+        ReclutaTI\Providers\EventServiceProvider::class,
+        ReclutaTI\Providers\RouteServiceProvider::class,
 
     ],
 
